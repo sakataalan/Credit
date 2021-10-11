d88e7584c5e4c7e352f0550ab729d4d1951e7e04
Alan Sakata <sakataalan@iMac-Pro-de-Alan.local>
2021-10-11 16:51:22 -0300

    clear git cache

--- src/App.java
+++ src/App.java
@@ -1,53 +1,66 @@
 import java.util.Scanner;
 
 public class App {
-
-    static long takeNumber() {
-        Scanner scan = new Scanner(System.in);
-        long ccNumber;
-
-        do {
-            System.out.println("Digite o número do cartão: ");
-            ccNumber = scan.nextLong();
-        } while (ccNumber <= 0);
-
-        scan.close();
-        return ccNumber;
-    }
-
-    static int sumOfSecondToLastDigit(long ccNumber) {
-        ccNumber /= 10;
-        int sum = 0;
-
-        while (ccNumber > 0) {
-            sum += (((ccNumber % 10) * 2) / 10) + (((ccNumber % 10) * 2) % 10);
-            ccNumber /= 100;
-        }
-
-        return sum;
-    }
-
-    static int sumOfRemainDigits(long ccNumber) {
-        int sum = 0;
-
-        while (ccNumber > 0) {
-            sum += ccNumber % 10;
-            ccNumber /= 100;
-        }
-
-        return sum;
-    }
-
     public static void main(String[] args) {
-        long ccNumber = takeNumber();
-        int sum1 = sumOfSecondToLastDigit(ccNumber);
-        int sum2 = sumOfRemainDigits(ccNumber);
-        int result = sum1 + sum2;
+        long creditCardNumber = scanNumber();
+        int oddNumbers = sumOfSecondToLastDigit(creditCardNumber);
+        int evenNumbers = sumOfRemainDigits(creditCardNumber);
+        boolean isValid = result(oddNumbers, evenNumbers);
 
-        if (result % 10 == 0) {
+        if (isValid) {
             System.out.println("valido");
         } else {
             System.out.println("invalido");
         }
     }
+    
+    static long scanNumber() {
+        
+        Scanner scan = new Scanner(System.in);
+        long creditCardNumber = 0;
+        boolean isNaN;
+
+        do {
+            try {
+                System.out.println("Digite o número do cartão: ");
+                creditCardNumber = scan.nextLong();
+                isNaN = false;
+            } catch (Exception e) {
+                System.out.println("Apenas números");
+                scan.nextLine();
+                isNaN = true;
+            }
+        } while (isNaN || creditCardNumber <= 0);
+        
+        scan.close();
+        return creditCardNumber;
+    }
+
+    static int sumOfSecondToLastDigit(long creditCardNumber) {
+        creditCardNumber /= 10;
+        int sum = 0;
+
+        while (creditCardNumber > 0) {
+            sum += (((creditCardNumber % 10) * 2) / 10) + (((creditCardNumber % 10) * 2) % 10);
+            creditCardNumber /= 100;
+        }
+
+        return sum;
+    }
+
+    static int sumOfRemainDigits(long creditCardNumber) {
+        int sum = 0;
+
+        while (creditCardNumber > 0) {
+            sum += creditCardNumber % 10;
+            creditCardNumber /= 100;
+        }
+
+        return sum;
+    }
+
+    static boolean result(int oddNumbers, int evenNumbers) {
+        return (oddNumbers + evenNumbers) % 10 == 0; 
+    }
+
 }
